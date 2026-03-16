8358094cf31f0a2a917bf16a7561028c9cdc9eab
AlexNasc <alexramos.nasc1@gmail.com>
2026-03-16 17:07:36 -0300

    Add Exercise12.c for zodiac sign calculation

---
+++ Lista04-Estrutura_de_Seleção/Exercise12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int Dia, Mes;
+    char Aries, Touro, Gemeos, Cancer, Leao, Virgem, Libra, Escorpiao, Sagitario, Capricornio, Aquario, Peixes;
+    
+    scanf("%d %d", Dia, Mes);
+    
+    if(Dia >= 21 && dia <= 20)
+    
+    return 0;
+}
