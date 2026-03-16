0b61c5cd2a61dde31d3fc4a411584020c5eb31e5
AlexNasc <alexramos.nasc1@gmail.com>
2026-03-15 22:35:17 -0300

    Add selection structure to the user's bus station.

--- Lista4-Estrutura_de_Seleção/Exercise04.c
+++ Lista4-Estrutura_de_Seleção/Exercise04.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello_word");
+    int X;
+    
+    scanf("%d", &X);
+    
+    if(X == 1){
+        printf("Bela Vista");
+    }
+    else if(X == 2){
+        printf("Figueira");
+    }
+    else if(X == 3){
+        printf("Sta Terezinha");
+    }
+    else{
+        printf("Código inexistente");
+    }
 
     return 0;
 }
