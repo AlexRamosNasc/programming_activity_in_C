46c0f0539490e16e3ee4a47bc0c508b90fd7f92a
AlexNasc <alexramos.nasc1@gmail.com>
2026-03-23 19:30:21 -0300

    Update and rename Exercise01.c to Exercise01A.c

--- Lista00-Extras/Exercise01A.c
+++ Lista00-Extras/Exercise01A.c
@@ -3,11 +3,11 @@
 int main(){
     int X;
     float Salario, Media, Soma = 0;
-    for(X = 1; X <=3; X++){
+    for(X = 1; X <=150; X++){
         scanf("%f", &Salario);
         Soma = Soma + Salario;
     }
-    Media = Soma / 3;
+    Media = Soma / 150;
     printf("Media de Salario: %.2f\n", Media);
 
     return 0;
