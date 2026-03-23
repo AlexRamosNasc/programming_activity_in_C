d1640d40393481021fc035ada61b876e8dbaebf4
AlexNasc <alexramos.nasc1@gmail.com>
2026-03-23 19:29:15 -0300

    Create Exercise01.c

---
+++ Lista00-Extras/Exercise01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int X;
+    float Salario, Media, Soma = 0;
+    for(X = 1; X <=3; X++){
+        scanf("%f", &Salario);
+        Soma = Soma + Salario;
+    }
+    Media = Soma / 3;
+    printf("Media de Salario: %.2f\n", Media);
+
+    return 0;
+}
