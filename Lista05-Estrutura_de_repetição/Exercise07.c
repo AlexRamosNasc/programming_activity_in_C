7840c439c70f2de19071bd1111f8b25ef1546cac
AlexNasc <alexramos.nasc1@gmail.com>
2026-03-30 16:31:17 -0300

    Fix input loop and update gender statistics calculations

--- Lista05-Estrutura_de_repetição/Exercise07.c
+++ Lista05-Estrutura_de_repetição/Exercise07.c
@@ -4,9 +4,9 @@ int main(){
     char Sexo;
     float Macho = 0, Femea = 0, Pmacho, MMidade, Pfemea, MFidade, TMidade = 0, TFidade = 0, Idade;
 
-    for(int I = 1; I <= 6; I++){
+    for(int I = 1; I <= 15; I++){
         
-        scanf("%c %f", &Sexo, &Idade);
+        scanf(" %c %f", &Sexo, &Idade);
         
         if(Sexo == 'M'){
             Macho++;
@@ -17,13 +17,29 @@ int main(){
             TFidade += Idade;
         }
     }
-Pmacho = (Macho / 6) * 100;
+
+if(Macho == 0){
+    Pfemea = (Femea / 15) * 100;
+    MFidade = TFidade / Femea;
+    printf("Não há homens no grupo\nPorcentagem de pessoas do sexo feminino: %.2f%%\nMédia de idade do sexo feminino: %.2f\n", Pfemea, MFidade);
+    
+    return 0;
+}
+if(Femea == 0){
+    Pmacho = (Macho / 15) * 100;
+    MMidade = TMidade / Macho;
+    printf("Porcentagem de pessoas do sexo masculino: %.2f%%\nMédia de idade do sexo masculino: %.2f\nNão há mulheres no grupo\n", Pmacho, MMidade);
+    
+    return 0;
+}
+
+Pmacho = (Macho / 15) * 100;
 MMidade = TMidade / Macho;
-Pfemea = (Femea / 6) * 100;
+Pfemea = (Femea / 15) * 100;
 MFidade = TFidade / Femea;
-    
-printf("Porcentagem de pessoas do sexo masculino: %.2f%%\nMédia de idade do sexo masculino: %.2f\nPorcentagem de pessoas do sexo feminino: %.2f%%\nMédia de idade do sexo feminino: %.2f\n", Pmacho, MMidade, Pfemea, MFidade );
-    
+
+printf("Porcentagem de pessoas do sexo masculino: %.2f%%\nMédia de idade do sexo masculino: %.2f\n", Pmacho, MMidade);
+printf("Porcentagem de pessoas do sexo feminino: %.2f%%\nMédia de idade do sexo feminino: %.2f\n", Pfemea, MFidade);
+
 return 0;
 }
-TERMINAR
