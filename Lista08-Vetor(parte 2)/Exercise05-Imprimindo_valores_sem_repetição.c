65006273ff33de657a3763672fcd0ac566ad13a9
AlexNasc <alexramos.nasc1@gmail.com>
2026-04-21 19:29:06 -0300

    Prevent duplicate values in input array
    Fix logic to prevent duplicate entries in the array.

--- Lista08-Vetor(parte 2)/Exercise05-Imprimindo_valores_sem_repetição.c
+++ Lista08-Vetor(parte 2)/Exercise05-Imprimindo_valores_sem_repetição.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    int  n, cont = 0;
+    int A[64];
+    
+    scanf("%d", &n);
+    
+    while(cont < n){
+        int Repetido = 0;
+        
+        scanf("%d", &A[cont]);
+        
+        for(int i = 0; i < cont; i++){
+            if(A[cont] == A[i]){
+                Repetido = 1;
+                n = n - 1;
+                break;
+            }
+        }
+        if(Repetido == 0){
+            cont++;
+        }
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d ", A[i]);
+    }
+    
+    return 0;
+}
