88fc02751770161324aa858ae22536a8d991f582
AlexNasc <alexramos.nasc1@gmail.com>
2026-05-18 19:24:44 -0300

    Correct logic and input in questão04.c
    Fix logic errors and improve input handling in questão04.c

---
+++ Correção_Prova02/questão04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+  int n, i, x, cont = 0;
+  scanf("%d", &n);
+  int v[n];
+  for(i = 0; i < n; i++){
+    scanf("%d", &v[i]);
+  }
+  scanf("%d", &x);
+  for(i = 0; i < n; i++){
+    if(i = 0; i < n; i++){
+      cont++;
+    }
+  }
+  if(cont > 0){
+    printf("%d cadastrado &d vezes", n, cont);
+  }
+  else{
+    printf("%d não cadastrado", x);
+  }
+  return 0;
+}
+  
