844ab222f7be92e67e00935680a5955bf582deec
AlexNasc <alexramos.nasc1@gmail.com>
2026-06-25 14:25:54 -0300

    Change fun to accept pointers for x and y
    Refactor function to use pointers for swapping values.

--- Exercícios_de_ponteiro/Exercise03.c
+++ Exercícios_de_ponteiro/Exercise03.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
-int fun(int x, int y){
+void fun(int *px, int *py){
     int temp;
-    temp = y;
-    y = x;
-    x = temp;
+    temp = *py;
+    *py = *px;
+    *px = temp;
 
-    printf("x vale %d\n", x);
-    printf("E y vale %d", y);
+    printf("x vale %d\n", *px);
+    printf("E y vale %d", *py);
 }
 
 int main(){
     int x = 10, y = 3;
-    int *px, *py;
+    
+    printf("x vale %d\n", x);
+    printf("E y vale %d\n\n", y);
 
-    px = &x;
-    py = &y;
-
-    printf("x vale %d\n", *px);
-    printf("E y vale %d\n\n", *py);
-
-    fun(x, y);
+    fun(&x, &y);
 
    return 0;
 }
