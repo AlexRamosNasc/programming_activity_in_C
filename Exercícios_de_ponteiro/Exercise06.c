4ef0caa50c5cdcc81c450e530b21a4ccd5198457
AlexNasc <alexramos.nasc1@gmail.com>
2026-06-26 14:52:23 -0300

    Update array values in Exercise06.c

--- Exercícios_de_ponteiro/Exercise06.c
+++ Exercícios_de_ponteiro/Exercise06.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    float vet[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    float vet[10] = {1.01, 2.1, 3.2, 4.3, 5.4, 6.5, 7.6, 8.7, 9.8, 10.9};
     float *pv;
 
     for(int i = 0; i < 10; i++){
